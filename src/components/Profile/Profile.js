b8f42b2710c02261d891dcafe54126262c6ef0ff
TAVnotDove <user-email>
2022-08-02 13:00:49 +0300

    created basic profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,7 +3,16 @@ import "./Profile.css";
 
 const Profile = () => {
   return (
-    <p>Profile</p>
+    <div className="profile-div">
+      <div className="profile-details-div">
+        <label className="profile-details-label">Email:</label>
+        <p>example@domain.extension</p>
+      </div>
+      <div className="profile-details-div">
+        <label className="profile-details-label">Username:</label>
+        <p>User Name</p>
+      </div>
+    </div>
   );
 };
 
