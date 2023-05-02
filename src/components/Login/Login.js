bbc99f81e239bb215ae0715b3122b6b666531859
TAVnotDove <slavei.dikovski123@gmail.com>
2023-05-02 17:53:12 +0300

    fixed ESLint errors

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useState } from "react";
 import "./Login.css";
 import userLogin from "../../services/userServices/userLogin";
 import { Link, useNavigate } from "react-router-dom";
